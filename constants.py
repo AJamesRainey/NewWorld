26f554ce8399d5236c46d81a94cd1f278262024e
raineyatsea@gmail.com <raineyatsea@gmail.com>
2022-06-03 19:41:58 -0600

    added constants to its own file

---
+++ constants.py
@@ -0,0 +1,16 @@
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
+
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 0.25
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
+SPRITE_PIXEL_SIZE = 128
+GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
+MAP_NAME = "proto_map2.tmx"
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 10
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
